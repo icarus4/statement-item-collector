bf17c979e609895d5288c61223935cabef9d68de
icarus4 <icarus4.chu@gmail.com>
2014-07-31 16:58:45 +0800

    Add dependent :destroy to the self join of statement_items

--- app/models/statement_item.rb
+++ app/models/statement_item.rb
@@ -17,7 +17,7 @@ class StatementItem < ActiveRecord::Base
   # belongs_to :parent_statement_item, class_name: 'StatementItem'
 
   # replace self join by gem of closure_tree
-  acts_as_tree
+  acts_as_tree dependent: :destroy
 
   has_and_belongs_to_many :statements, join_table: :statement_items_statements
 
