d38b51c1aeffd68732544de66943f226d0b0fd8f
icarus4 <icarus4.chu@gmail.com>
2014-08-04 10:30:42 +0800

    [UI] shrink margin of table's tr 2. add stack class

--- app/controllers/comparisons_controller.rb
+++ app/controllers/comparisons_controller.rb
@@ -66,3 +66,23 @@ class TwseWebStatement
     return @doc
   end
 end
+
+
+class Stack
+  def initialize
+    @stack = Array.new
+  end
+
+  def pop
+    @stack.pop
+  end
+
+  def push(element)
+    @stack.push(element)
+    self
+  end
+
+  def size
+    @stack.size
+  end
+end
