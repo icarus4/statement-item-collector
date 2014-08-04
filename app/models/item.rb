f409e95a65559bb7b3ab751cf37ab7ee45003791
icarus4 <icarus4.chu@gmail.com>
2014-08-04 16:47:17 +0800

    update annotate info

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 # == Schema Information
 #
-# Table name: statement_items
+# Table name: items
 #
 #  id           :integer          not null, primary key
 #  name         :string(255)      not null
--- app/models/item_statement_pair.rb
+++ app/models/item_statement_pair.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: item_statement_pairs
+#
+#  id           :integer          not null, primary key
+#  item_id      :integer          not null
+#  statement_id :integer          not null
+#
+
 # Join model for Item and Statement
 class ItemStatementPair < ActiveRecord::Base
   belongs_to :item
