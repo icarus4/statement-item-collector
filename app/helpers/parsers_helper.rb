e6bc0624158a5a83ca09969ec411027e0ee8f362
icarus4 <icarus4.chu@gmail.com>
2014-08-08 13:39:06 +0800

    show parent item

--- app/helpers/parsers_helper.rb
+++ app/helpers/parsers_helper.rb
@@ -1,6 +1,6 @@
 module ParsersHelper
   def render_item(item, px_per_level)
     margin = (item.level - 1) * px_per_level
-    content_tag(:span, item.name, style: "margin-left: #{margin}px")
+    content_tag(:span, "#{item.name} <span style='color: lightgray'>（#{item.parent.name}）</span>".html_safe, style: "margin-left: #{margin}px")
   end
 end
