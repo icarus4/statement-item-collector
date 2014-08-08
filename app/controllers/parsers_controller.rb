e3d60af640f15ddb31b52ac17a9097eee1115eac
icarus4 <icarus4.chu@gmail.com>
2014-08-08 12:41:18 +0800

    1. 階層式顯示 stock => statements 2. toggleable 顯示內容

--- app/controllers/parsers_controller.rb
+++ app/controllers/parsers_controller.rb
@@ -14,6 +14,9 @@ class ParsersController < ApplicationController
 
   def all
     @items = Item.all.where.not(name: 'root')
+
+    # FIXME: this query sucks, should be improved later
+    @stocks = @items.map{|i|i.statements.map(&:stock).uniq}.uniq.flatten.uniq.sort_by{|stock|stock.ticker}
   end
 
 end
