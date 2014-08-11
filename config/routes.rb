ed33ada13caf7e145e329d002773395567d416f7
icarus4 <icarus4.chu@gmail.com>
2014-08-11 11:31:56 +0800

    1. add navbar at top 2. change root route

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-  root 'parsers#ifrs'
+  root 'parsers#index'
 
   get 'parsers' => 'parsers#ifrs'
   get 'parsers/parse/' => 'parsers#parse'
