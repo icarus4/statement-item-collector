1ca9637ee96b9065a542db310b0446fd94bedb86
icarus4 <icarus4.chu@gmail.com>
2014-08-21 17:04:59 +0800

    minor UI changes

--- app/models/item_stock_pair.rb
+++ app/models/item_stock_pair.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: item_stock_pairs
+#
+#  id       :integer          not null, primary key
+#  item_id  :integer          not null
+#  stock_id :integer          not null
+#
+
 class ItemStockPair < ActiveRecord::Base
   belongs_to :item
   belongs_to :stock
