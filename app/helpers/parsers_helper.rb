9d75a9ffe876f100b90ce6901787a4965e4c9956
icarus4 <icarus4.chu@gmail.com>
2014-08-22 11:51:20 +0800

    可顯示特定category or sub_category之資料: 顯示正確股票檔數

--- app/helpers/parsers_helper.rb
+++ app/helpers/parsers_helper.rb
@@ -7,4 +7,17 @@ module ParsersHelper
       content_tag(:span, "#{item.name}", style: "margin-left: #{margin}px")
     end
   end
+
+  def render_stock_count(item, category=nil, sub_category=nil)
+    stock_count = nil
+    if category.blank? && sub_category.blank?
+      stock_count = item.stocks.size
+    elsif category.present? && sub_category.blank?
+      stock_count = item.stocks.where(category: category).size
+    elsif category.present? && sub_category.present?
+      stock_count = item.stocks.where(category: category, sub_category: sub_category).size
+    end
+
+    content_tag(:span, "共 #{stock_count} 檔", class: 'stock-count toggleable-switch')
+  end
 end
