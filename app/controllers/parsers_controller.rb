631bb9784640f4bcf7aa19d850656fc9acc40849
icarus4 <icarus4.chu@gmail.com>
2014-09-02 13:14:34 +0800

    set year order

--- app/controllers/parsers_controller.rb
+++ app/controllers/parsers_controller.rb
@@ -5,6 +5,8 @@ class ParsersController < ApplicationController
   def index
     @stocks = Stock.all.order(:ticker)
     @not_parsed_stocks = Stock.get_not_parsed_stocks
+    @year_list = [["2007", 2007], ["2008", 2008], ["2009", 2009], ["2010", 2010], ["2011", 2011], ["2012", 2012], ["2013", 2013], [2014, 2014]]
+    @default_year = 2014
   end
 
   def parse
