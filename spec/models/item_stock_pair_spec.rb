ff9270121582df97f670d27c13750d643444d6fa
icarus4 <icarus4.chu@gmail.com>
2014-11-11 16:31:20 +0800

    update annotate info

--- spec/models/item_stock_pair_spec.rb
+++ spec/models/item_stock_pair_spec.rb
@@ -1,3 +1,12 @@
+# == Schema Information
+#
+# Table name: item_stock_pairs
+#
+#  id       :integer          not null, primary key
+#  item_id  :integer          not null
+#  stock_id :integer          not null
+#
+
 require 'rails_helper'
 
 RSpec.describe ItemStockPair, :type => :model do
--- spec/models/statement_spec.rb
+++ spec/models/statement_spec.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: statements
+#
+#  id         :integer          not null, primary key
+#  stock_id   :integer          not null
+#  year       :integer          not null
+#  quarter    :integer          not null
+#  created_at :datetime
+#  updated_at :datetime
+#  s_type     :string(255)
+#
+
 require 'spec_helper'
 
 describe Statement do
