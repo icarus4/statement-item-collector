e9935e345a5285eadf31bb74779347fe80c8e643
icarus4 <icarus4.chu@gmail.com>
2014-11-19 21:14:04 +0800

    order xbrl items by stocks count

--- app/controllers/standard_items_controller.rb
+++ app/controllers/standard_items_controller.rb
@@ -5,5 +5,10 @@ class StandardItemsController < ApplicationController
 
   def show
     @si = StandardItem.find(params[:id])
+    items = @si.items.where(item_standard_item_pairs: {is_exactly_matched: true})
+    @exactly_matched_items = items.sort_by {|i| i.stocks.size }.reverse
+
+    items = @si.items.where(item_standard_item_pairs: {is_exactly_matched: false})
+    @not_exactly_matched_items = items.sort_by {|i| i.stocks.size }.reverse
   end
 end
